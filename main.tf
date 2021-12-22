1ebd7edfb1b2569cb4aab8baa56cef241994ef1b
machamp0714 <rateliasudaisuki@gmail.com>
2021-12-22 21:49:17 +0900

    add resources

--- main.tf
+++ main.tf
@@ -10,3 +10,32 @@ terraform {
 provider "aws" {
   region = "ap-northeast-1"
 }
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "example" {
+  vpc_id            = aws_vpc.example.id
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "example" {
+  name   = "http_sg"
+  vpc_id = aws_vpc.example.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
